ad6c8652c0f31333df56a01e1a134af2f56e7715
drbaminh456 <dangbaminh260896@gmail.com>
2019-04-21 00:14:29 +0700

    Add header.

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', (req, res) => {
   var vm = {
-    layout: 'home.handlebars'
+    layout: 'main.handlebars'
   };
   res.render('home/index', vm);
 });
