483559160cf242042a1e7924045cfb404a81627a
drbaminh456 <dangbaminh260896@gmail.com>
2019-06-09 18:14:52 +0700

    add register function

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var SHA256 = require('crypto-js/sha256');
+var accountRepo = require('../repos/accountRepo');
 
 router.get('/login', (req, res) => {
   var vm = {
@@ -12,6 +14,31 @@ router.get('/register', (req, res) => {
     layout: 'log.handlebars'
   };
   res.render('register/register', vm);
+});
+
+router.post('/register-success', (req, res) => {
+  // var account = {
+  //   Email: req.body.Email,
+  //   Password: SHA256(req.body.password).toString()
+  // };
+
+  // accountRepo.getMaxID().then(rows => {
+    console.log('xxxxxxxxxxxx');
+    
+    var account = {
+      Email: req.body.Email,
+      Password: SHA256(req.body.password).toString(),
+      First_Name: req.body.firstName,
+      Last_Name: req.body.lastName,
+      Birthdate: req.body.birthDate
+    };
+    accountRepo.add(account);
+    // req.session.idAccount = customer.Account_ID;
+  // });
+  var vm = {
+    layout: 'log.handlebars'
+  };
+  res.render('register/register-success', vm);
 });
 router.get('/writer/post', (req, res) => {
   var vm = {
---
+++ repos/accountRepo.js
@@ -0,0 +1,12 @@
+var db = require('../fn/db');
+
+exports.add = acc => {
+    var sql = `insert into account(email, password,	first_name,last_name, birthdate) values
+    ('${acc.Email}', '${acc.Password}', '${acc.First_Name}', '${acc.Last_Name}', '${acc.Birthdate}', 'subscriber')`;
+    return db.save(sql);
+}
+
+exports.getMaxID = () => {
+    var sql = `select MAX(account_id) AS idmax from account`;
+    return db.load(sql);
+}
